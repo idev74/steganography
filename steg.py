a679cb987d9811125d7f15c7b8ad2f875607b7cc
idev74 <idevassy77@gmail.com>
2023-11-18 11:50:09 -0800

    can write text and encode an image

--- steg.py
+++ steg.py
@@ -1,4 +1,4 @@
-from PIL import Image
+from PIL import Image, ImageDraw,ImageFont
 
 
 def decode_image(path_to_png):
@@ -31,17 +31,54 @@ def decode_image(path_to_png):
     decoded_image.save("decoded_image.png")
 
 
-def encode_image(path_to_png):
+def encode_image(path_to_png, hidden_text):
     """
     Encodes an image hidden in an image using binary.
     """
-    pass
+    # Open the image using PIL:
+    original_image = Image.open(path_to_png)
+    encoded_image = original_image.copy()
+    pixels = encoded_image.load()
+    x_size, y_size = original_image.size
 
+    # Create a new PIL image with the same size as the encoded image:
+    image_text = write_text(hidden_text, x_size, y_size)
+    text_pixels = image_text.load()
 
-def write_text(text_to_write):
+    # Iterate over the pixels of both images and modify the LSB of the original image
+    for x in range(x_size):
+        for y in range(y_size):
+            original_pixel = original_image.getpixel((x, y))
+            text_pixel = text_pixels[x, y]
+            
+            # Modify the LSB of the red channel of the original image
+            new_red_channel = (original_pixel[0] & 0xFE) | (text_pixel[0] >> 7)
+            new_pixel = (new_red_channel, original_pixel[1], original_pixel[2])
+            
+            pixels[x, y] = new_pixel
+    
+    # Save the encoded image to disk:
+    encoded_image.save("encoded_image.png")
+
+def write_text(text_to_write, width, height):
     """
-    TODO: Add docstring and complete implementation.
+    Writes text to an image and returns the image.
     """
-    pass
+    # Create a blank black image
+    img = Image.new('RGB', (width, height), color='black')
+    create = ImageDraw.Draw(img)
 
-decode_image('encoded_sample.png')
+    # Adjust the font size as needed
+    font_size = 100
+    font = ImageFont.truetype('/Library/Fonts/Arial.ttf', font_size)
+    
+    create.text((10, 10), text_to_write, font=font, fill='white')
+
+    # Save the image to disk
+    img.save('text.png')
+    return img
+    
+encode_image('reef.png', 'Credit to Francesco Ungaro on Unsplash!')
+
+# Will be the same as 'text.png
+decode_image('encoded_image.png')
